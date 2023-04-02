0c56af658a218e4a91dd72472bf666ec32e64af9
Mykyta Kovalenko <nikkv.ottf@gmail.com>
2023-04-02 20:04:19 +0200

    Lab1: adjust neurons

--- Lab1/main.py
+++ Lab1/main.py
@@ -7,10 +7,8 @@ z = np.array([0, 1, 1, 0])
 
 if __name__ == '__main__':
   model = keras.Sequential()
-  model.add(keras.layers.Dense(2, input_dim=2, activation="relu"))
-  model.add(keras.layers.Dense(1, activation="relu"))
-
-  print(model.summary())
+  model.add(keras.layers.Dense(15, input_dim=2, activation="sigmoid"))
+  model.add(keras.layers.Dense(1, activation="sigmoid"))
 
   model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
   model.fit(x, y, epochs=3000, verbose=0)
